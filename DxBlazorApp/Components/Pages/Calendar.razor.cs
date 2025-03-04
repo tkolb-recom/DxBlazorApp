536b97be49ed3f61ac8c3318fd766892d7161b8e
Tobias Kolb <tkolb@recom.eu>
2025-03-04 09:10:06 +0100

    ICS upload

--- DxBlazorApp/Components/Pages/Calendar.razor.cs
+++ DxBlazorApp/Components/Pages/Calendar.razor.cs
@@ -1,3 +1,4 @@
+using DevExpress.Blazor;
 using DxBlazorApp.Data;
 using Microsoft.AspNetCore.Components;
 
@@ -5,7 +6,14 @@ namespace DxBlazorApp.Components.Pages;
 
 public partial class Calendar : ComponentBase
 {
+    [Inject] private CalendarService CalendarService { get; set; }
+
     private CalendarEntry[]? _entries = null;
+
+    private DxWindow windowRef;
+    ElementReference popupTarget;
+    bool windowVisible;
+
     public DateTime Begin { get; set; } = DateTime.Today;
     public DateTime End { get; set; } = DateTime.Today.AddDays(1);
 
@@ -21,7 +29,31 @@ public partial class Calendar : ComponentBase
 
     private void SaveEntry()
     {
-        CalendarService.AddEntry(this.Begin, this.End); 
+        CalendarService.AddEntry(this.Begin, this.End);
         _ = this.LoadEntries();
+    }
+
+    private void OpenUploadWindow()
+    {
+        if (windowVisible)
+            windowRef.CloseAsync();
+        else
+            windowRef.ShowAtAsync(popupTarget);
+    }
+
+
+    protected void SelectedFilesChanged(IEnumerable<UploadFileInfo> files)
+    {
+        UploadVisible = files.ToList().Count > 0;
+        InvokeAsync(StateHasChanged);
+    }
+
+    public bool UploadVisible { get; set; }
+
+    protected async Task OnFilesUploading(FilesUploadingEventArgs args)
+    {
+        var file = args.Files[0];
+        using var stream = new System.IO.MemoryStream();
+        await file.OpenReadStream(file.Size).CopyToAsync(stream);
     }
 }
